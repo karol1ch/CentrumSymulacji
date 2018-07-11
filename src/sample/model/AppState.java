5213de1f524dcea1dc7ccdfcb31cc2f0d1d8dcf2
lukasz <lukasz.aleksiejew@gmail.com>
2018-07-11 23:48:56 +0200

    wywalenie loadScenario...

--- src/sample/model/AppState.java
+++ src/sample/model/AppState.java
@@ -21,17 +21,6 @@ public class AppState {
         allScenarios = new LinkedList<>();
     }
 
-    public Scenario loadScenario() throws IOException {
-        File file = new File("Scenariusze" + File.separator + "ScenariuszPierwszy.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
-        String description = reader.readLine();
-        ArrayList<String> components = new ArrayList<>(Arrays.asList(description.split(";")));
-        reader.close();
-        Scenario scenario = new Scenario();
-        scenario.setName(components.get(0));
-        return scenario;
-    }
-
     public Scenario getScenarioToShow() {
         return scenarioToShow;
     }
